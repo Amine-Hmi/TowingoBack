da7706eca3ba6619fe8ffd47e0a8519728345b25
Amine-Hmi <hammemi.amine@gmail.com>
2021-05-17 05:30:11 +0100

    +2 car apis/swagger doc

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 //* Log queries with morgan 
 
 var morgan = require("morgan");
-app.use(morgan("dev"));
+app.use(morgan("tiny"));
 
 app.use(express.json());
 app.use(
@@ -19,6 +19,18 @@ app.use(
     extended: true,
   })
 );
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const swaggerUi = require('swagger-ui-express');
+
+
+const swaggerDocument = require('./swagger.json');
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
  //* CONNECT TO DATABASE //
 
@@ -31,29 +43,39 @@ const con = mysql.createConnection({
 
 //? TUNNEL LOCALHOST TO WEB //
 
-// (async () => {
-//   const tunnel = await localtunnel({
-//     port: 4545,
-//     subdomain: process.env.LT_SUBDOMAIN,
-//   });
-//   tunnel.url;
-//   console.info(tunnel.url);
-//   tunnel.on("close", () => {
-//     console.log("Tunnel closed");
-//   });
-// })();
+(async () => {
+  const tunnel = await localtunnel({
+    port: 4545,
+    subdomain: process.env.LT_SUBDOMAIN,
+  });
+  tunnel.url;
+  console.info(tunnel.url);
+  tunnel.on("close", () => {
+    console.log("Tunnel closed");
+  });
+})();
 
 const server = app.listen(4545, () => {
   let host = server.address().address;
   let port = server.address().port;
-  console.log(`Started on PORT ${port}`);
+  console.log(`🚀 Started on PORT ${port}`);
 });
 
+//* LOG REQUEST PERFORMANCES *//
+const getDurationInMilliseconds = (start) => {
+  const NS_PER_SEC = 1e9
+  const NS_TO_MS = 1e6
+  const diff = process.hrtime(start)
 
-//? ESTABLISH CONNECTION TO DATABASE //
+  return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS
+}
+
+
+
+//? ESTABLISH CONNECTION TO DATABASE // 
 
 con.connect((err) => {
-  if (err) console.log("Database Connection Error:", err.sqlMessage);
+  if (err) console.log("Database Connection Error:", err.code);
   else {
     console.log("Successfully connected to DB 😀");
   }
@@ -71,63 +93,104 @@ app.get("/api/users", (req, res) => {
   con.query("select * from users", (error, rows, fields) => {
     if (error) console.log("users query error:" + error);
     else {
-      res.send(
-        "<ul>" +
-          rows.map(
-            (user) =>
-              `<li style=\"list-style:none\"><strong style=\"color:blue\">Email address:</strong> ${user.email_address}</li>
-               <li style=\"list-style:none\"><strong style=\"color:blue\">Phone number:</strong> ${user.phone_number}</li>
-               <li style=\"list-style:none\"><strong style=\"color:blue\">Creation date:</strong> ${user.created}</li>
-               <li style=\"list-style:none\"><strong style=\"color:blue\">Premium:</strong> ${user.premium}</li>
-               <li style=\"list-style:none\"><strong style=\"color:blue\">Verified:</strong> ${user.verified}</li>`
-          ) +
-          "</ul>"
-      );
-      // res.send(rows);
+      //res.send(
+      //   `<style>
+      //   table {
+      //     border-collapse: collapse;
+      //     border: 2px solid rgb(200, 200, 200);
+      //     letter-spacing: 1px;
+      //     font-family: sans-serif;
+      //     font-size: .8rem;
+      // }
+      
+      // td,
+      // th {
+      //     border: 1px solid rgb(190, 190, 190);
+      //     padding: 5px 10px;
+      // }
+      
+      // td {
+      //     text-align: center;
+      // }
+      
+      // </style>
+      // <table>
+      //     <thead>
+      //         <tr>
+      //             <th>id</th>
+      //             <th>E-mail address</th>
+      //             <th>Phone Number</th>
+      //             <th>Created</th>
+      //             <th>Premium</th>
+      //             <th>Premium</th>
+      //         </tr>
+      //     </thead>
+      //     <tbody>
+      //         <tr>` +
+      //     rows.map(
+      //       (user) =>
+      //         `<tr>
+      //         <td>${user.id}</td>
+      //         <td>${user.email_address}</td>
+      //         <td>${user.phone_number}</td>
+      //         <td>${user.created}</td>
+      //         <td>${user.premium}</td>
+      //         <td>${user.verified}</td>
+  
+      //     </tr>`)+`</tbody> </table> `
+      // );
+       res.send(rows);
     }
   });
 });
 
 app.get("/", (req, res) => {
-  res.send(`<h1>Welcome to TOWINGO 😀</h1><a href="/api/users">View users<a/>`);
+  res.send(`<h1 style="text-align:center"> Welcome to Towingo 🚗</h1><a href="/api/docs">📚 View API documentation<a/>`);
 });
 
 
-async function isEmailValid(email) {
-  return emailValidator.validate(email);
+async function isEmailValid(email_address) {
+  try {
+     let isValid = await emailValidator.validate(email_address);
+     return isValid
+  }catch(err){
+    res.send(err)
+  }
 }
 
-const registerUser = (email, phone, res) => {
+const registerUser = async (email_address, phone_number, res) => {
   let fetchUserCount =
     "SELECT count(*) as count FROM `users` WHERE `phone_number`=" +
-    con.escape(phone) +
+    con.escape(phone_number) +
     "and `email_address`=" +
-    con.escape(email) +
+    con.escape(email_address) +
     "";
   // let sql = `INSERT INTO users ('?', '?', '?', '?', '?', '?') VALUES [NULL, ${mail}, ${phone},${CURRENT_TIMESTAMP},'0', '0']`;
   let addUserReq =
     "INSERT INTO `users` (`id`, `username` ,`email_address`,`phone_number` ,`DOB`, `created`, `verified`, `premium`) VALUES (NULL,NULL," +
-    con.escape(email) +
+    con.escape(email_address) +
     "," +
-    con.escape(phone) +
+    con.escape(phone_number) +
     ", NULL, " +
     con.escape(CURRENT_TIMESTAMP) +
     ",'0', '0')";
 
   let fetchUser =
     "select exists (  select `email_address` from users where `email_address` =" +
-    con.escape(email) +
+    con.escape(email_address) +
     " or phone_number =" +
-    con.escape(phone) +
+    con.escape(phone_number) +
     ") as result ";
 
 //* FETCH USER BY PHONE NUMBER OR E-mail
 
   con.query(fetchUser, (err, result) => {
-    if (err) return "DB fetch user failed" + err;
+    if (err)
+      return "DB fetch user failed" + err;
+
+
     //* if fetch is success
-    else
-     if (result[0].result === 0) {
+    else if (result[0].result === 0) {
       con.query(addUserReq, (err, result) => {
         if (err) {
           return res
@@ -139,32 +202,32 @@ const registerUser = (email, phone, res) => {
             .send({ message: "Success: " + result.insertId });
         }
       });
-    } else return res.status(409).send({ message: "user already exists" });
+    } else { return res.status(409).send({ message: "user already exists" }); }
   });
 };
 
 //* REGISTER A NEW USER //
 
-app.post("/api/register/", async function (req, res, next) {
-  const { email, phone } = req.body;
+app.post("/api/user/register/", async function (req, res, next) {
+  const { email_address, phone_number } = req.body;
 
-  const { valid, reason, validators } = await isEmailValid(email);
+  const { valid, reason, validators } = await isEmailValid(email_address);
 
   //? CHECK EMAIL SYNATX IS VALID//
 
   if (valid) {
-    const { message } = registerUser(email, phone, res);
-    // return (message)
+    const { message } = registerUser(email_address, phone_number, res);
+    return (message)
   } else return res.status(400).send({ message: validators[reason].reason });
 });
 
 //* Log existing USER //
 
-app.post("/api/login/", async function (req, res, next) {
-  const { phone } = req.body;
+app.post("/api/user/login/", async function (req, res, next) {
+  const { phone_number } = req.body;
 
   let fetchPhone =
-    "select exists ( select `phone_number` from users where `phone_number` =" + con.escape(phone) +") as result ";
+    "select exists ( select `phone_number` from users where `phone_number` =" + con.escape(phone_number) +") as result ";
 
     con.query(fetchPhone, (err, result) => {
       if (err) return "Error: " + err.message;
@@ -211,7 +274,7 @@ authenticator.options = { digits: 4 };
 totp.options = { digits: 4, step: 90 }; // *step is token validity in seconds [default:30] //
 
 const secret = authenticator.generateSecret();
-function genToken() {
+function genOTP() {
   try {
     const otptoken = totp.generate(secret);
     const tUsed = totp.timeUsed();
@@ -228,17 +291,14 @@ function genToken() {
 }
 
 app.get("/api/otp", async (req, res) => {
-  let token = genToken();
-  console.log(
-    "token: " + token.otptoken,
-    "expires in: " + token.tRemain + " seconds"
-  );
+  let token = genOTP();
   res.json(token);
+  console.log("token: " + token.otptoken,"expires in: " + token.tRemain + " seconds");
 });
 //*GET USER INFOS //
-app.post("/api/userinfos", async function (req, res, next) {
+app.post("/api/user/infos", async function (req, res, next) {
   const { phone_number } = req.body;
-  con.query("select * from users where phone_number = "+con.escape(phone_number)+"", (error, row, fields) => {
+  con.query(`SELECT id, email_address, username, phone_number, gender, DATE_FORMAT(created,'%d/%m/%Y %H:%i:%s') as created, verified, premium, DATE_FORMAT(DOB,'%d/%m/%Y') as DOB FROM users WHERE  phone_number = ${phone_number}`, (error, row, fields) => {
     if (error) console.log("users query error:" + error);
     else {
       res.json(row);
@@ -247,18 +307,19 @@ app.post("/api/userinfos", async function (req, res, next) {
 })
 
 //* Update user infos //
-app.patch("/api/edituser/:id", async function (req, res, next) {
+app.patch("/api/user/:id/edit", async function (req, res, next) {
   const { DOB,username,gender } = req.body;
   const id = req.params.id
   con.query("UPDATE users SET username = ?, gender = ?, DOB = ? where id = ?" ,[username,gender,DOB,id], (error,result) => {
     if (error) console.log("users query error:" + error);
     else {
-      res.send(result);
+      res.status(200)
+      .send(`User infos updated successfully:\n{ username: "${username}", gender: "${gender}", DOB: ${DOB} }`);
     }
   });
 })
-// * Get List of Vehicle vendors
-app.get("/api/vendors", (req, res) => {
+// * Get List of Vehicle make
+app.get("/api/make", (req, res) => {
   con.query("select * from car2dbmakes", (error, rows, fields) => {
     if (error) console.log("users query error:" + error);
     else {
@@ -273,12 +334,45 @@ app.get("/api/vendors", (req, res) => {
 });
 
 // * Get List of Vehicle vendor models
-app.get("/api/models/:maker", (req, res) => {
-  const maker = req.params.maker
-  con.query(`select * from car2dbmodels where make_id = ${maker}`, (error, rows, fields) => {
+app.get("/api/car/models/:make", (req, res) => {
+  const make = req.params.make
+  con.query(`select * from car2dbmodels where make_id = ${make}`, (error, rows, fields) => {
     if (error) console.log("users query error:" + error);
     else {
       res.send(rows);
       }
   });
 });
+
+// * Get List of cars owned by user
+app.get("/api/cars/:user", (req, res) => {
+  const user = req.params.user
+  con.query(`select car2dbmakes.make_name, car2dbmodels.model_name, isdefault from user_cars inner join car2dbmakes on
+  user_cars.make_id = car2dbmakes.make_id 
+  inner join car2dbmodels on user_cars.model_id = car2dbmodels.model_id
+  where user_cars.user_id = ${user} order by user_cars.car_id asc `, (error, rows, fields) => {
+    if (error) console.log("users query error:" + error);
+    else {
+      res.send(rows);
+      }
+  });
+});
+
+//*Add a new user car *//
+app.post("/api/car/add/:user", async function (req, res, next) {
+  let {make_id, model_id} = req.body;
+  let user_id = req.params.user
+  con.query( `INSERT INTO user_cars (make_id, model_id, user_id, isdefault,car_id) VALUES (${make_id},${model_id},${user_id},false,NULL)`, (err, result) => {
+    if (err) return "DB fetch user failed" + err;
+        if (err) {
+          return res
+            .status(404)
+            .send({ message: "SQL add query failed " + err.message });
+        } else {
+          console.log(JSON.stringify(result))
+          return res
+            .status(200)
+            .send({ message: `car successfully added to user car collection` });
+        }
+      })
+    })
